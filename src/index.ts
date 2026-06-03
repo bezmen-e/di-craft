4b0f2b7948d77b4106da319426fd490d474ef602
Егор Безмен <egor1911@gmail.com>
2026-06-03 14:26:48 +0300

    chore: add provider module imports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
 export type { Token } from "./token";
 export { createToken } from "./token";
+
+export type {
+	DepsMap,
+	Factory,
+	FactoryProvider,
+	Provider,
+	ResolveDeps,
+	TokenValue,
+	ValueProvider,
+} from "./provider";
+
+export {
+	isFactoryProvider,
+	isValueProvider,
+	provideFactory,
+	provideValue,
+} from "./provider";
