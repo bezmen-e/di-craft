e8cddde84e84e953391603664a7ae4627c24671f
Егор Безмен <egor1911@gmail.com>
2026-06-03 16:16:32 +0300

    fix: drop inner module public imports

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,5 @@ export {
 	provideFactory,
 	provideValue,
 } from "./provider";
-export type { Registry } from "./registry";
-export { createRegistry, DuplicateProviderError } from "./registry";
 export type { Token } from "./token";
 export { createToken } from "./token";
